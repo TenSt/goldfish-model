93b1c5d2697a3ec00685a6f12577d7013c3cd295
TenSt <stepan.maks@gmail.com>
2019-12-16 22:53:59 +0200

    adding json new

--- main.go
+++ main.go
@@ -157,14 +157,14 @@ func getJSONData() {
 				num, err := strconv.Atoi(v)
 				checkError("error atoi:\n", err)
 				if num <= 35 {
-					l[j] = "35"
-					v = "35"
+					l[j] = "0"
+					v = "0"
 				} else if num > 35 && num < 40 {
-					l[j] = "40"
-					v = "40"
+					l[j] = "1"
+					v = "1"
 				} else if num >= 40 {
-					l[j] = "45"
-					v = "45"
+					l[j] = "2"
+					v = "2"
 				}
 				if a := stringInSlice(v, categories); a == false {
 					categories = append(categories, v)
